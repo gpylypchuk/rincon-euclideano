8276416b25ed49e5fbf6a7868d231c1ef0d067e0
gpylypchuk <geronimopylypchuk@gmail.com>
2023-07-13 00:05:33 -0300

    :package: add math notation

---
+++ src/data/constants.ts
@@ -0,0 +1,14 @@
+interface IColors {
+  black: `#${string}`;
+  white: `#${string}`;
+  purple: `#${string}`;
+  gray: `#${string}`;
+}
+
+// Russian purple: Mountain majesty
+export const COLORS: IColors = {
+  black: "#252525;",
+  white: "#EEEEEE",
+  purple: "#786fa6",
+  gray: "#596275",
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
+import { COLORS } from "./src/data/constants";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "cool-purple": COLORS.purple,
+      },
+    },
+  },
+  plugins: [],
+};
